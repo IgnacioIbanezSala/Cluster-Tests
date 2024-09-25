693463eee0cd11e0da717e0df46d595b56a36f45
Ignacio I S <ignacio.ibanez@mi.unc.edu.ar>
2024-09-24 22:09:36 -0300

    Changed cartpole from v0 to v1

--- Cartpolev1.py
+++ Cartpolev1.py
@@ -3,7 +3,8 @@ from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras  import Sequential
 import numpy as np
 import matplotlib.pyplot as plt
-import gym
+import gymnasium as gym
+
 
 HIDDEN_SIZE = 128
 
@@ -19,18 +20,19 @@ model.compile(
     optimizer="adam",
     metrics=["accuracy"])
 
-env=gym.make("CartPole-v1",new_step_api=True)
-env.reset()
+env=gym.make("CartPole-v1")
+env.reset()[0]
+
+
 
 def RandomAgent(obs):
   return env.action_space.sample()
 
 def CrossEntropyAgent(obs):
-  prob=model.predict(np.array([obs]),verbose=0)[0][0] # Probability that the action to be 1
-  a=np.random.choice([1,0],p=[prob,1-prob])
+  prob=model.predict(obs, verbose=0)# Probability that the action to be 1
+  a=np.random.choice([1,0],p=[prob[0][0],1-prob[0][0]])
   return a
 
-S=set()
 n=0
 for j in range(50):
   # Collect data
@@ -39,16 +41,18 @@ for j in range(50):
   for e in range(episodes):
     done=False
     trunc=False
-    obs=env.reset()
-    S.add(tuple(obs))
+    obs=env.reset()[0]
+  
     n+=1
     trajectory=[]
     while not done and not trunc:
+      obs = np.reshape(obs, [1,env.observation_space.shape[0]])
       action=CrossEntropyAgent(obs)
       n_obs,r,done,trunc,info = env.step(action)
       trajectory.append((obs,action,r))
-      obs=n_obs
-      S.add(tuple(obs))
+      
+      
+
       n+=1
       trajectories.append(trajectory)
     #print('iteration',j,'episode',e,'Return =',len(trajectory))
@@ -57,13 +61,12 @@ for j in range(50):
   for i,trajectory in enumerate(trajectories):
    Ri[i]=len(trajectory)
   i=np.argmax(Ri)
-  print('iteration',j,'states visited',len(S),'Mean Return =',Ri.mean(),)
+  print('iteration',j, 'Mean Return =',Ri.mean(),)
   # Training
   X=np.array([s for s,a,r in trajectories[i]])
   y=np.array([a for s,a,r in trajectories[i]])
-  history = model.fit(X, y, epochs=100, verbose=0)
 
-print(n,len(S))
+#print(n,len(S))
 
 one=False
 trunc=True
