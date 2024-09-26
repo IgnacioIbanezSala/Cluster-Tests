f3632166efae538520722852f21e221670b80fe2
Ignacio I S <ignacio.ibanez@mi.unc.edu.ar>
2024-09-26 10:50:54 -0300

    Changes

--- Cartpolev1.py
+++ Cartpolev1.py
@@ -8,7 +8,7 @@ import gymnasium as gym
 
 HIDDEN_SIZE = 128
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 tf.debugging.set_log_device_placement(True)
 
