a608830121de3967e392163d35afcedeb322ead3
Ignacio I S <ignacio.ibanez@mi.unc.edu.ar>
2024-09-26 10:52:39 -0300

    Changes

--- Cartpolev1.py
+++ Cartpolev1.py
@@ -8,9 +8,9 @@ import gymnasium as gym
 
 HIDDEN_SIZE = 128
 
-#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-tf.debugging.set_log_device_placement(True)
+#tf.debugging.set_log_device_placement(True)
 
 # Neuronal Net
 model = Sequential([
